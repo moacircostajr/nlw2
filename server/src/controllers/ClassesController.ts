8a28bcb7cda7e75cf788d5842f3e8887ec6dff35
Moacir Costa <moacircostajr@gmail.com>
2021-02-14 11:06:03 -0300

    listagem com filtros

--- server/src/controllers/ClassesController.ts
+++ server/src/controllers/ClassesController.ts
@@ -16,7 +16,7 @@ export default class ClassesController {
 
     const filters = request.query
     const subject = filters.subject as string
-    const week_day = filters.subject as string
+    const week_day = filters.week_day as string
     const time = filters.time as string
 
     if (!filters.week_day || !filters.subject || !filters.time) {
@@ -27,7 +27,18 @@ export default class ClassesController {
 
     const timeInMinutes = convertHourToMinutes(time)
 
-    const classes = await db('classes').where('classes.subject', '=', subject)
+    const classes = await db('classes')
+      .whereExists(function () {
+        this.select('class_schedule.*')
+          .from('class_schedule')
+          .whereRaw('`class_schedule`.`class_id` = `classes`.`id`')
+          .whereRaw('`class_schedule`.`week_day` = ??', [Number(week_day)])
+          .whereRaw('`class_schedule`.`from` <= ??', [timeInMinutes])
+          .whereRaw('`class_schedule`.`to` > ??', [timeInMinutes])
+      })
+      .where('classes.subject', '=', subject)
+      .join('users', 'classes.user_id', '=', 'users.id')  // inner join
+      .select(['classes.*', 'users.*'])
     return response.json(classes)
   }
 
