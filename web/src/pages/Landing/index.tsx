e6c11972742f03f616b5bf6c76813230ea120d1c
Moacir Costa <moacircostajr@gmail.com>
2021-02-14 01:09:21 -0300

    rotas e lista de professores disponíveis

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -6,6 +6,7 @@ import studyIcon from '../../assets/images/icons/study.svg'
 import giveClassesIcon from '../../assets/images/icons/give-classes.svg'
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg'
 import './styles.css'
+import { Link } from 'react-router-dom'
 
 
 interface Props {
@@ -22,15 +23,15 @@ const Landing = (props: Props) => {
         </div>
         <img src={landingImg} alt="Plataforma de estudos" className="hero-image" />
         <div className="buttons-container">
-          <a href="#" className="study">
+          <Link to="/study" className="study">
             <img src={studyIcon} alt="Estudar" />
             Estudar
-          </a>
+          </Link>
 
-          <a href="#" className="give-classes">
+          <Link to="give-classes" className="give-classes">
             <img src={giveClassesIcon} alt="Dar aulas" />
             Dar aulas
-          </a>
+          </Link>
         </div>
         <span className="total-connections">
           Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo" />
---
+++ web/src/pages/TeacherForm/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+
+}
+
+const TeacherForm = (props: Props) => {
+  return (
+    <div>
+      <h1>Teacher Form</h1>
+    </div>
+  )
+}
+
+export default TeacherForm
