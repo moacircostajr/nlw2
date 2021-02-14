029cbc2302a51e34066757436dbe5fc71cfd30bf
Moacir Costa <moacircostajr@gmail.com>
2021-02-14 11:09:35 -0300

    inicia connections controller

---
+++ server/src/controllers/ConnectionsController.ts
@@ -0,0 +1,8 @@
+import { Request, Response } from 'express'
+
+export default class ConnectionsController {
+
+  async index(request: Request, response: Response) { }
+  async create(request: Request, response: Response) { }
+
+}
